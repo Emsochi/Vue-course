b099cadb06e6074e0cad53ef01aadbd21f82ba6f
Esochi <sochiera.emilia@gmail.com>
2023-03-30 21:25:00 +0200

    form bindings

---
+++ HTTP request methods/Declarative Rendering/Form Bindings.vue
@@ -0,0 +1,23 @@
+Używając v-bindi v-onrazem, możemy tworzyć dwukierunkowe powiązania na elementach wejściowych formularza:
+
+<input :value="text" @input="onInput">
+
+function onInput(e) {
+  text.value = e.target.value
+}
+
+np.
+<script setup>
+import { ref } from 'vue'
+
+const text = ref('')
+
+function onInput(e) {
+  text.value = e.target.value
+}
+</script>
+
+<template>
+  <input :value="text" @input="onInput" placeholder="Type here">
+  <p>{{ text }}</p>
+</template>
