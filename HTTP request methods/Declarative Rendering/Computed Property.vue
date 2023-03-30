756f7ccb363fd04d84b30b86f1d4cdc596b6a47c
Esochi <sochiera.emilia@gmail.com>
2023-03-30 23:17:08 +0200

    computed property

---
+++ HTTP request methods/Declarative Rendering/Computed Property.vue
@@ -0,0 +1,9 @@
+Kontynuujmy budowanie na szczycie listy rzeczy do zrobienia z ostatniego kroku. 
+Tutaj dodaliśmy już funkcję przełączania do każdego zadania. 
+Odbywa się to poprzez dodanie donewłaściwości do każdego obiektu todo i użycie v-modeldo powiązania go z polem wyboru:
+
+<li v-for="todo in todos">
+    <input type="checkbox" v-model="todo.done">
+    ...
+  </li>
+  
