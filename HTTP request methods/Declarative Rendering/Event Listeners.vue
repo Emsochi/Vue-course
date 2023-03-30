67ccd33714fa9637f048cc2d88b37ce7320b74f8
Esochi <sochiera.emilia@gmail.com>
2023-03-30 21:18:15 +0200

    event listeners

---
+++ HTTP request methods/Declarative Rendering/Event Listeners.vue
@@ -0,0 +1,23 @@
+Możemy nasłuchiwać zdarzeń DOM za pomocą v-ondyrektywy:
+
+<button v-on:click="increment">{{ count }}</button>
+
+Ze względu na częste użycie v-onma również skróconą składnię:
+
+<button @click="increment">{{ count }}</button>
+
+np.
+
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+function increment() {
+  count.value++
+}
+</script>
+
+<template>
+  <button @click = "increment">{{count}}</button>
+</template>
