4eb7d87ad1f0bce6122e3ac65015fa407be09962
Esochi <sochiera.emilia@gmail.com>
2023-03-30 21:11:09 +0200

    attribute bindings

---
+++ HTTP request methods/Declarative Rendering/Attribute Bindings.vue
@@ -0,0 +1,19 @@
+Aby powiązać atrybut z wartością dynamiczną, używamy dyrektywy v-bind
+
+<div v-bind:id="dynamicId"></div>
+
+<script setup>
+import { ref } from 'vue'
+
+const titleClass = ref('title')
+</script>
+
+<template>
+  <h1 :class="titleClass">Make me red</h1>
+</template>
+
+<style>
+.title {
+  color: red;
+}
+</style>
