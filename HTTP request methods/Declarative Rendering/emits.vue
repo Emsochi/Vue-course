a491cf5d861de470d54dcae2bc6a207be345bb02
Esochi <sochiera.emilia@gmail.com>
2023-03-31 22:48:56 +0200

    emits

---
+++ HTTP request methods/Declarative Rendering/emits.vue
@@ -0,0 +1,11 @@
+Oprócz otrzymywania rekwizytów komponent potomny może również emitować zdarzenia do rodzica:
+
+<script setup>
+// declare emitted events
+const emit = defineEmits(['response'])
+
+// emit with argument
+emit('response', 'hello from child')
+</script>
+
+Pierwszym argumentem jest nazwa zdarzenia. Wszelkie dodatkowe argumenty są przekazywane do detektora zdarzeń.emit()
