992c14704e73867f01f6a89a2f1f764698ddf4db
Esochi <sochiera.emilia@gmail.com>
2023-03-31 22:39:07 +0200

    changes

---
+++ HTTP request methods/Declarative Rendering/Lifecycle and Template Refs.vue
@@ -0,0 +1,31 @@
+Możemy poprosić o ref szablonu - czyli odniesienie do elementu w szablonie - używając specjalnego refatrybutu :
+
+<p ref="p">hello</p>
+
+Aby uzyskać dostęp do ref, musimy zadeklarować ref o pasującej nazwie:
+
+const p = ref(null)
+
+Zauważ, że ref jest inicjowany nullwartością. Dzieje się tak, ponieważ element jeszcze nie istnieje, gdy jest wykonywany. Szablon ref jest dostępny tylko po zamontowaniu komponentu .<script setup>
+
+//Aby uruchomić kod po zamontowaniu, możemy użyć onMounted()funkcji:
+
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  // component is now mounted.
+})
+
+//<script setup>
+//import { ref, onMounted } from 'vue'
+
+const p = ref(null)
+
+onMounted(() => {
+  p.value.textContent = 'mounted!'
+})
+</script>
+
+<template>
+  <p ref="p">hello</p>
+</template>
