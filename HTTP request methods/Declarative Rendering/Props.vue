999f2e667ef5e5657bc77312d04a51073a0fab0e
Esochi <sochiera.emilia@gmail.com>
2023-03-31 22:45:34 +0200

    props

---
+++ HTTP request methods/Declarative Rendering/Props.vue
@@ -0,0 +1,8 @@
+Komponent potomny może przyjmować dane wejściowe od rodzica za pośrednictwem właściwości . Najpierw musi zadeklarować rekwizyty, które akceptuje:
+
+<!-- ChildComp.vue -->
+<script setup>
+const props = defineProps({
+  msg: String
+})
+</script>
