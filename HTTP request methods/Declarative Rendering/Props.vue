708f9db7912a74be7fb10006f4f05034123e4a97
Esochi <sochiera.emilia@gmail.com>
2023-03-31 22:46:42 +0200

    + defineProps

--- HTTP request methods/Declarative Rendering/Props.vue
+++ HTTP request methods/Declarative Rendering/Props.vue
@@ -6,3 +6,9 @@ const props = defineProps({
   msg: String
 })
 </script>
+
+Note defineProps()to makro w czasie kompilacji i nie trzeba go importować. Po zadeklarowaniu msgrekwizytu można go użyć w szablonie komponentu potomnego. Dostęp do niego można również uzyskać w JavaScript za pośrednictwem zwróconego obiektu defineProps().
+
+Rodzic może przekazać rekwizyt dziecku, tak jak atrybuty. Aby przekazać wartość dynamiczną, możemy również użyć v-bindskładni:
+
+<ChildComp :msg="greeting" />
