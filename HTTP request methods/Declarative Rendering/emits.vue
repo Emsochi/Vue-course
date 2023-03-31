68a4e7a3ddbf3e93cf682e829128dd3c043003d6
Esochi <sochiera.emilia@gmail.com>
2023-03-31 22:51:10 +0200

    example

--- HTTP request methods/Declarative Rendering/emits.vue
+++ HTTP request methods/Declarative Rendering/emits.vue
@@ -13,3 +13,27 @@ Pierwszym argumentem jest nazwa zdarzenia. Wszelkie dodatkowe argumenty są prze
 Rodzic może nasłuchiwać zdarzeń emitowanych przez dziecko za pomocą v-on- tutaj program obsługi otrzymuje dodatkowy argument z wywołania emisji dziecka i przypisuje go do stanu lokalnego:
 
 <ChildComp @response="(msg) => childMsg = msg" />
+
+    np.
+
+<script setup>
+import { ref } from 'vue'
+import ChildComp from './ChildComp.vue'
+
+const childMsg = ref('No child msg yet')
+</script>
+
+<template>
+  <ChildComp />
+  <p>{{ childMsg }}</p>
+</template>
+
+<script setup>
+const emit = defineEmits(['response'])
+
+emit('response', 'hello from child')
+</script>
+
+<template>
+  <h2>Child component</h2>
+</template>
