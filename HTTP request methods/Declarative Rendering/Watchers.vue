8554e97f938bf0deb5c31d0a8861bfb490ca9acb
Esochi <sochiera.emilia@gmail.com>
2023-03-31 22:41:52 +0200

    changes watchers

--- HTTP request methods/Declarative Rendering/Watchers.vue
+++ HTTP request methods/Declarative Rendering/Watchers.vue
@@ -8,3 +8,32 @@ watch(count, (newCount) => {
   // yes, console.log() is a side effect
   console.log(`new count is: ${newCount}`)
 })
+
+watch()może bezpośrednio oglądać ref, a wywołanie zwrotne jest uruchamiane za każdym razem, gdy countzmienia się wartość ' s . 
+watch()może również oglądać inne typy źródeł danych
+
+<script setup>
+import { ref, watch } from 'vue'
+
+const todoId = ref(1)
+const todoData = ref(null)
+
+async function fetchData() {
+  todoData.value = null
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/todos/${todoId.value}`
+  )
+  todoData.value = await res.json()
+}
+
+fetchData()
+
+watch(todoId, fetchData)
+</script>
+
+<template>
+  <p>Todo id: {{ todoId }}</p>
+  <button @click="todoId++">Fetch next todo</button>
+  <p v-if="!todoData">Loading...</p>
+  <pre v-else>{{ todoData }}</pre>
+</template>
