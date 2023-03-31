af3411a96220a687a6b8dc8a5b4ad71e02998692
Esochi <sochiera.emilia@gmail.com>
2023-03-31 22:40:24 +0200

    watchers

---
+++ HTTP request methods/Declarative Rendering/Watchers.vue
@@ -0,0 +1,10 @@
+Czasami może być konieczne reaktywne wykonanie „skutków ubocznych” - na przykład zalogowanie numeru do konsoli, gdy się zmieni. Możemy to osiągnąć dzięki obserwatorom:
+
+import { ref, watch } from 'vue'
+
+const count = ref(0)
+
+watch(count, (newCount) => {
+  // yes, console.log() is a side effect
+  console.log(`new count is: ${newCount}`)
+})
