81a7b9f22bb15dcb00a8f8b29114a50216aaa7e7
Esochi <sochiera.emilia@gmail.com>
2023-04-01 21:16:45 +0200

    directive

--- Getting Started/Attribute Bindings.vue
+++ Getting Started/Attribute Bindings.vue
@@ -2,6 +2,10 @@ Aby powiązać atrybut z wartością dynamiczną, używamy dyrektywy v-bind
 
 <div v-bind:id="dynamicId"></div>
 
+Dyrektywa to specjalny atrybut, który zaczyna się od przedrostkav- . 
+Są częścią składni szablonów Vue. 
+Podobnie jak w przypadku interpolacji tekstu, wartości dyrektywy są wyrażeniami JavaScript, które mają dostęp do stanu komponentu. 
+
 <script setup>
 import { ref } from 'vue'
 
