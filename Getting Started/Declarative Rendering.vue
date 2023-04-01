46f5ac7fd7d999db8fc2a3a5f3c7cd7109e6b03b
Esochi <sochiera.emilia@gmail.com>
2023-04-01 21:14:21 +0200

    example

--- Getting Started/Declarative Rendering.vue
+++ Getting Started/Declarative Rendering.vue
@@ -30,3 +30,17 @@ Treść wewnątrz wąsów nie ogranicza się tylko do identyfikatorów lub ście
 
 szablon
 <h1>{{ message.split('').reverse().join('') }}</h1>
+
+np.
+
+<script setup>
+import { reactive, ref } from 'vue'
+
+const counter = reactive({ count: 0 })
+const message = ref('Hello World!')
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+  <p>Count is: {{ counter.count }}</p>
+</template>
