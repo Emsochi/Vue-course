27bb77c1e71dc29bcb7c0134458eb2b6be3d01c1
Esochi <sochiera.emilia@gmail.com>
2023-04-01 21:11:47 +0200

    titles and place change

---
+++ HTTP request methods/Getting Started/Components.vue
@@ -0,0 +1,17 @@
+Do tej pory pracowaliśmy tylko z jednym komponentem. Aplikacje Real Vue są zwykle tworzone za pomocą zagnieżdżonych komponentów.
+
+Komponent nadrzędny może renderować inny komponent w swoim szablonie jako komponent podrzędny. Aby użyć komponentu potomnego, musimy go najpierw zaimportować:
+
+import ChildComp from './ChildComp.vue'
+
+Następnie możemy użyć komponentu w szablonie jako:
+
+<ChildComp />
+
+<script setup>
+import ChildComp from './ChildComp.vue'
+</script>
+
+<template>
+  <ChildComp />
+</template>
