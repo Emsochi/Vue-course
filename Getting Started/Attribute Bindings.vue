0d9cb3c986426edf060a6a9d153ef38f2a35d7eb
Esochi <sochiera.emilia@gmail.com>
2023-04-01 21:18:20 +0200

    shorthand syntax

--- Getting Started/Attribute Bindings.vue
+++ Getting Started/Attribute Bindings.vue
@@ -6,6 +6,15 @@ Dyrektywa to specjalny atrybut, który zaczyna się od przedrostkav- .
 Są częścią składni szablonów Vue. 
 Podobnie jak w przypadku interpolacji tekstu, wartości dyrektywy są wyrażeniami JavaScript, które mają dostęp do stanu komponentu. 
 
+Część po dwukropku ( :id) jest „argumentem” dyrektywy. 
+Tutaj atrybut elementu idzostanie zsynchronizowany z dynamicIdwłaściwością ze stanu komponentu.
+
+Ponieważ v-bindjest używany tak często, ma dedykowaną skróconą składnię:
+
+<div :id="dynamicId"></div>
+
+
+example:
 <script setup>
 import { ref } from 'vue'
 
