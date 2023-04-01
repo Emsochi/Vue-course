e53c4cd4c488198e6075f4411d0276d0535ae6c5
Esochi <sochiera.emilia@gmail.com>
2023-04-01 21:03:35 +0200

    slots

---
+++ HTTP request methods/Declarative Rendering/slots.vue
@@ -0,0 +1,5 @@
+Oprócz przekazywania danych przez rekwizyty, komponent nadrzędny może również przekazywać dziecku fragmenty szablonu za pośrednictwem slotów :
+
+<ChildComp>
+    This is some slot content!
+  </ChildComp>
