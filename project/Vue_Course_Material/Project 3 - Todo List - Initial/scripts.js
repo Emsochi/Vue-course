2103bce45fc52c48f83bf108dbdc30c71000bcd3
Esochi <sochiera.emilia@gmail.com>
2023-04-06 22:38:06 +0200

    undone functionality

--- project/Vue_Course_Material/Project 3 - Todo List - Initial/scripts.js
+++ project/Vue_Course_Material/Project 3 - Todo List - Initial/scripts.js
@@ -1,19 +1,8 @@
-var todos = [
-    {
-        text: 'Learn HTML,CSS and Javascript',
-        done: true
-    },
-    {
-        text: 'Learn the basics of Vue JS',
-        done: false
-    }
-   
-]
 
 const todosApp = {
     data() {
         return {
-            todos:  window.todos,
+            todos:  [],
             newTodo: {
                 done: false
             }
